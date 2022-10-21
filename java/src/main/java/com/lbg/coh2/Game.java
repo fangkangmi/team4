944545796c2c110e173bbd295bd47526aad60873
Aditya Menon <menon.or.adithya@gmail.com>
2022-10-21 13:38:13 +0100

    Create variable sized grid

--- java/src/main/java/com/lbg/coh2/Game.java
+++ java/src/main/java/com/lbg/coh2/Game.java
@@ -1,5 +1,19 @@
 package com.lbg.coh2;
 
+import java.util.Random;
+
+import java.util.Random;
+
 public class Game {
-    boolean grid[][] = new boolean[30][30];
+    boolean grid[][];
+    int treasureSpot[] = new int[2];
+
+
+    void createGrid(int length, int width){
+        this.grid = new boolean[width][length];
+        Random gen = new Random();
+        treasureSpot[0] = gen.nextInt(0, length);
+        treasureSpot[1] = gen.nextInt(0, width);
+        this.grid[treasureSpot[1]][treasureSpot[0]] = true;
+    }
 }
